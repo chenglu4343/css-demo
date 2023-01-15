a4d60cf1a9fc108c6e7d7b8755ae4f0926f016f9
chenglu <chenglu4343@qq.com>
2023-01-15 11:52:30 +0800

    ✨ feat: add demo1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,58 @@
 <template>
-    <h1>Demo1</h1>
+  <h1>Demo1</h1>
+  <div class="color-container">
+    <div v-for="(color, index) of red" :key="color" class="color-item" :style="{
+      '--bg-color': color,
+      '--text-color': index >= 5 ? 'white' : 'rgba(0,0,0,0.85)',
+      backgroundColor: 'var(--bg-color)',
+      color: 'var(--text-color)',
+    }">
+      <span class="color-text">red-{{ index }}</span>
+      <span class="color-value">{{ color }}</span>
+    </div>
+  </div>
 </template>
   
 <script setup lang='ts'>
-  
+import { red } from '@ant-design/colors'
 </script>
   
-<style>
-  
+<style scoped>
+.color-container {
+  --item-add-width: 12px;
+  --border-radius: 5px;
+  --transition-duration: .3s;
+  --left-width: 5px;
+  width: 300px;
+  border-radius: var(--border-radius);
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.color-container:hover .color-value {
+  left: 0;
+  color: var(--text-color)
+}
+
+.color-item {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 15px;
+  overflow: hidden;
+  margin-right: var(--item-add-width);
+  transition: all var(--transition-duration);
+}
+
+.color-item:hover {
+  margin-right: calc(- var(--item-add-width));
+  border-radius: 0 var(--border-radius) var(--border-radius) 0;
+}
+
+.color-value {
+  opacity: 0.7;
+  transition: all var(--transition-duration);
+  position: relative;
+  left: var(--left-width);
+  color: var(--bg-color);
+}
 </style>
